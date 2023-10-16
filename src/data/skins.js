d89de2ea2b84da43fc64df667129ee886bb12cf5
Ivan L'olivier <ivan.lolivier@gmail.com>
2023-10-16 12:41:13 -0300

    Adding IvanLolivier skin

--- src/data/skins.js
+++ src/data/skins.js
@@ -1,19 +1,24 @@
-const skins = [{
+const skins = [
+  {
     title: "Oh My Console",
     url: "https://raw.githubusercontent.com/chrisvdev/CVTalk/main/public/assets/BackEnd-Skin.css"
-},
-{
+  },
+  {
     title: "Like Python",
     url: "https://raw.githubusercontent.com/chrisvdev/CVTalk/main/public/assets/BackEnd-Skin2.css"
-},
-{
+  },
+  {
     title: "Pythonesa's skin",
     url: "https://raw.githubusercontent.com/chrisvdev/CVTalk/main/public/assets/cssnesa.css"
-},
-{
+  },
+  {
     title: "Snakeblack's skin",
     url: "https://raw.githubusercontent.com/Snakeblack/chatbox-css/main/style.css"
-}
+  },
+  {
+    title: "IvanLolivier's skin",
+    url: "https://raw.githubusercontent.com/ivanlolivier/IvanLolivierCVTalkTheme/main/IvanLolivierCVTalkTheme.css"
+  }
 ]
 
 export default skins
